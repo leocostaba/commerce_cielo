23f9cdc899f2b83338df148839a47d368f86a3f5
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2015-07-30 19:46:46 -0400

    Added webservice-1.5-php requirement to composer.json

--- src/Checkout/Order.php
+++ src/Checkout/Order.php
@@ -2,53 +2,17 @@
 
 namespace CieloCheckout;
 
-$order = new stdClass();
-
-$order->OrderNumber = '1234';
-$order->SoftDescriptor = 'Nome que aparecerá na fatura';
-$order->Cart = new stdClass();
-$order->Cart->Discount = new stdClass();
-$order->Cart->Discount->Type = 'Percent';
-$order->Cart->Discount->Value = 10;
-$order->Cart->Items = array();
-$order->Cart->Items[0] = new stdClass();
-$order->Cart->Items[0]->Name = 'Nome do produto';
-$order->Cart->Items[0]->Description = 'Descrição do produto';
-$order->Cart->Items[0]->UnitPrice = 100;
-$order->Cart->Items[0]->Quantity = 2;
-$order->Cart->Items[0]->Type = 'Asset';
-$order->Cart->Items[0]->Sku = 'Sku do item no carrinho';
-$order->Cart->Items[0]->Weight = 200;
-$order->Shipping = new stdClass();
-$order->Shipping->Type = 'Correios';
-$order->Shipping->SourceZipCode = '14400000';
-$order->Shipping->TargetZipCode = '11000000';
-$order->Shipping->Address = new stdClass();
-$order->Shipping->Address->Street = 'Endereço de entrega';
-$order->Shipping->Address->Number = '123';
-$order->Shipping->Address->Complement = '';
-$order->Shipping->Address->District = 'Bairro da entrega';
-$order->Shipping->Address->City = 'Cidade da entrega';
-$order->Shipping->Address->State = 'SP';
-$order->Shipping->Services = array();
-$order->Shipping->Services[0] = new stdClass();
-$order->Shipping->Services[0]->Name = 'Serviço de frete';
-$order->Shipping->Services[0]->Price = 123;
-$order->Shipping->Services[0]->DeadLine = 15;
-$order->Payment = new stdClass();
-$order->Payment->BoletoDiscount = 0;
-$order->Payment->DebitDiscount = 10;
-$order->Customer = new stdClass();
-$order->Customer->Identity = 11111111111;
-$order->Customer->FullName = 'Fulano Comprador da Silva';
-$order->Customer->Email = 'fulano@email.com';
-$order->Customer->Phone = '11999999999';
-$order->Options = new stdClass();
-$order->Options->AntifraudEnabled = false;
-
 class Order extends Commons {
 
-  public $OrderNumber,
+  public
+    /**
+	 * Merchant's order number.
+	 */
+	$OrderNumber,
+
+    /**
+	 * Text shown on the buyer's bill right after merchant's name.
+	 */
 	$SoftDescriptor,
 	$Cart,
 	$Shipping,
@@ -56,11 +20,16 @@ class Order extends Commons {
 	$Customer,
 	$Options;
 
-  private $requirements = [
-	'properties' => [
-	  'Cart',
-	  'Shipping',
+  protected $property_requirements = [
+	'Cart' => [
+	  'empty' => ['negate' => FALSE],
+	],
+	'Shipping' => [
+	  'empty' => ['negate' => FALSE],
 	],
   ];
 
+  protected function set_Cart(Cart $Cart) {
+	$this->Cart = $Cart;
+  }
 }
